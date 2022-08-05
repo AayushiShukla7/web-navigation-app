152e60e0a4ed9ce87112aad65a230080383db365
AayushiShukla7 <AayushiShukla27@gmail.com>
2022-08-05 14:45:20 +0530

    Added query string and API key.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+const url = require("url");
 
 require("dotenv").config();
 
@@ -12,8 +14,23 @@ const app = express();
 app.use(cors());
 
 // routes
-app.get('/api/search', (req, res) => {
-    res.json({ success: "true" });
+app.get('/api/search/:query', async (req, res) => {
+    try {
+        // add api key & query strings
+        const params = new URLSearchParams({
+            access_token: process.env.API_KEY,
+            ...url.parse(req.url, true).query,
+        });
+
+        const query = req.params.query;
+        const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`);
+        console.log(params);
+        const data = results.data;
+        res.status(200).json(data);
+    }
+    catch(err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.listen(PORT, () => console.log(`app has started on port: ${PORT}`));
