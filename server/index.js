2b16103328d6b897bdb8f2b840baf02f469d6e2e
AayushiShukla7 <AayushiShukla27@gmail.com>
2022-08-05 17:04:20 +0530

    Moved routing to separate file inside a folder.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
-const url = require("url");
 
 require("dotenv").config();
 
@@ -14,23 +12,6 @@ const app = express();
 app.use(cors());
 
 // routes
-app.get('/api/search/:query', async (req, res) => {
-    try {
-        // add api key & query strings
-        const params = new URLSearchParams({
-            access_token: process.env.API_KEY,
-            ...url.parse(req.url, true).query,
-        });
-
-        const query = req.params.query;
-        const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`);
-        console.log(params);
-        const data = results.data;
-        res.status(200).json(data);
-    }
-    catch(err) {
-        res.status(500).json({ error: err.message });
-    }
-});
+app.use('/api/search/', require('./routes/searchResults.js'));
 
 app.listen(PORT, () => console.log(`app has started on port: ${PORT}`));
