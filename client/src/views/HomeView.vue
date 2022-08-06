ab8853c59c484fa4b667223f0a7e3eb7cfdeac16
AayushiShukla7 <AayushiShukla27@gmail.com>
2022-08-06 19:11:05 +0530

    Leaflet Map added.

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,14 +1,60 @@
 <template>
-  <div class="home">
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import leaflet from "leaflet";
+import { onMounted, ref } from "vue";
 
 export default {
   name: 'HomeView',
-  components: {
+  components: {},
+  setup() {
+    let map;
+    onMounted(() => {
+      // init map
+      map = leaflet.map('map').setView([28.538336, -81.379234], 10);
+
+      // add tile layer 
+      leaflet.tileLayer(
+          `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`,
+          {
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: process.env.VUE_APP_API_KEY,
+          }
+        )
+        .addTo(map);
+
+        getGeoLocation();
+    }) 
+
+    //const coords = ref(null);
+    const fetchCoords = ref(null);
+    //const geoMarker = ref(null);
+
+    const getGeoLocation = () => {
+      fetchCoords.value = true;
+      navigator.geolocation.getCurrentPosition(setCoords, getLocError);
+    };
+
+    const setCoords = (pos) => {
+      //console.log(pos);
+
+      // stop fetching coords
+      fetchCoords.value = null;
+
+      // set coords in session storage
+      
+    };
+
+    const getLocError = (err) => {
+      console.log(err);
+    };
   }
 }
 </script>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  jit: true,
   theme: {
     extend: {},
   },
